bade665956e0d05ef14f87dc89f36f7a181ab1bc
unknown <galan1451@gmail.com>
2019-07-22 15:24:34 -0500

    paths created

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function(app){
+    app.get("/survey", function(require, response){
+        response.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("*", function(require, response){
+        response.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
