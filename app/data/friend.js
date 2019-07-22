57ccecbf676053bb76d22a29d92833c54d64ce78
unknown <galan1451@gmail.com>
2019-07-22 15:42:48 -0500

    data array created

---
+++ app/data/friend.js
@@ -0,0 +1,18 @@
+var friendArr = [
+    {
+        name: "Rachel",
+        photo: "../public/images/rachel.jpg",
+        scores: [
+            5, 1, 2, 3, 1, 2, 5, 4, 2, 1
+        ]
+    },
+    {
+        name: "Phoebe",
+        photo: "../public/images/phoebe.jpg",
+        scores: [
+            4, 3, 3, 1, 2, 5, 4, 3, 2, 5
+        ]
+    }
+];
+
+module.exports = friendArr;
